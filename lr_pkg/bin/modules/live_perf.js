0a5591fd3f8650c8728f57f95f42588728aa609a
ratetanginamo <168286942+ratetanginamo@users.noreply.github.com>
2025-11-19 08:05:53 +0800

    Create live_perf.js

---
+++ lr_pkg/bin/modules/live_perf.js
@@ -0,0 +1,8 @@
+// live_perf.js (skeleton)
+module.exports = {
+  init: ()=>console.log('[LivePerf] initialized'),
+  onFileChange: (projectPath, filename) => {
+    // Placeholder: real perf metrics require injected client code.
+    console.log('[LivePerf] change', filename);
+  }
+};
