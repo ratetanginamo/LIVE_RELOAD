a1593050ea9a81b3b847b640295a17b87c392425
ratetanginamo <168286942+ratetanginamo@users.noreply.github.com>
2025-11-19 08:08:41 +0800

    Create mobile_preview.js

---
+++ lr_pkg/bin/modules/mobile_preview.js
@@ -0,0 +1,7 @@
+// mobile_preview.js (skeleton)
+module.exports = {
+  init: ()=>console.log('[MobilePreview] initialized'),
+  onFileChange: (projectPath, filename) => {
+    console.log('[MobilePreview] update preview for', filename);
+  }
+};
